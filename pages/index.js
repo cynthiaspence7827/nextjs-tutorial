52b2cc02255be7f55d1f2279a25575c0a59fadf8
cynthiaspence7827 <cynthiaspence7827@gmail.com>
2021-08-22 03:07:18 -0400

    Assets, Metadata, and CSS

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,20 @@
-import Head from "next/head";
-import Link from "next/link";
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
 
 export default function Home() {
   return (
-    <div className="container">
+    <Layout home>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>{siteTitle}</title>
       </Head>
-      <h1>
-        Read{" "}
-        <Link href="/posts/first-post">
-          <a>this page</a>
-        </Link>
-      </h1>
-    </div>
-  );
+      <section className={utilStyles.headingMd}>
+        <p>I am a Software engineer</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  )
 }
